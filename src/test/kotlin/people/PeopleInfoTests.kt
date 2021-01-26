a363ef998e7820c33c967d683d8cd7a8fb2896fb
Egor Rachkovskii <egor.rachkovskii@gmail.com>
2021-01-27 01:18:16 +0300

    Added getSpecificPersonInfoTest

--- src/test/kotlin/people/PeopleInfoTests.kt
+++ src/test/kotlin/people/PeopleInfoTests.kt
@@ -11,6 +11,7 @@ import com.google.gson.Gson
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertAll
 import pojo.People
+import pojo.Person
 
 class PeopleInfoTests {
     private val baseUrl = "https://swapi.dev/api/"
@@ -76,6 +77,60 @@ class PeopleInfoTests {
                     assertEquals(
                         "Checking that last person hair color is $lastPersonHairColor",
                         lastPersonHairColor, results.last().hairColor
+                    )
+                }
+            )
+        }
+    }
+
+    @Test
+    @DisplayName("Testing if /people/:id response is correct")
+    fun getSpecificPersonInfoTest() {
+        val personName = "Lando Calrissian"
+        val birthYear = "31BBY"
+        val personHomeworld = "http://swapi.dev/api/planets/30/"
+        val wasCreated = "2014-12-15T12:56:32.683000Z"
+        val personId = 25
+
+        val specificPersonPath = GET_PEOPLE_INFO.endPoint + personId
+        val response = get(baseUrl + specificPersonPath)
+
+        with(response) {
+            assertEquals(
+                "Checking that statusCode is ${SUCCESS.status}",
+                SUCCESS.status, statusCode()
+            )
+            assertTrue(
+                "Checking that contentType is JSON",
+                contentType().contains(JSON.toString())
+            )
+        }
+        val objectFromResponseJson = Gson().fromJson(response.asString(), Person::class.java)
+
+        with(objectFromResponseJson) {
+            assertAll("Checking that response contains correct data",
+                {
+                    assertEquals(
+                        "Checking that persons name is $personName",
+                        personName, name
+                    )
+                },
+                {
+                    assertEquals(
+                        "Checking that persons birth year is $birthYear",
+                        birthYear, birth_year
+                    )
+                },
+                {
+                    assertEquals(
+                        "Checking that persons homeworld is $personHomeworld",
+                        personHomeworld, homeworld
+                    )
+                },
+                {
+                    assertEquals(
+                        "Checking that person was created at $wasCreated",
+                        wasCreated, created
                     )
                 }
             )
